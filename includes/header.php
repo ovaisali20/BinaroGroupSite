0177df8676f89a6216a99911472d9bad7f9c25f0
Syed haris <haris.syed@binaro.io>
2023-06-05 20:55:04 +0500

    Header developed

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,39 @@
-<header></header>
+<header>
+    <div class="header_inner">
+        <div class="container">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 col-md-3 col-sm-12">
+                    <div class="mobile_logo">
+                        <a href="https://media.binaro.io/"><img src="/assets/images/logo.png" class="logo" /></a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12">
+                    <nav class="mobile-navigation">
+                        <ul>
+                            <li><a href="javascript:;">units</a></li>
+                            <li><a href="javascript:;">About Us</a></li>
+                            <li><a href="javascript:;">Portfolio</a></li>
+                            <li><a href="javascript:;">Resources</a></li>
+                            <li><a href="javascript:;">Blogs</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="col-lg-5 col-md-5 col-sm-12">
+                    <div class="header-button">
+                        <ul>
+                            <li><a href="javascript:;">Partner program</a></li>
+                            <li><a href="javascript:;">Request Callback</a></li>
+                            <li><a href="javascript:;">Get a Quote</a></li>
+                            <li><a href="javascript:;"><i class="fa fa-bars" aria-hidden="true"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
