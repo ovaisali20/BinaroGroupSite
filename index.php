a36cc2c8ea807ac78d318d9dcc5a0a0c794aac2f
Syed haris <haris.syed@binaro.io>
2023-06-09 22:27:00 +0500

    building/customization/transformation sections developed

--- index.php
+++ index.php
@@ -8,63 +8,216 @@
     </head>
     <body>
         <?php include("includes/header.php") ?>
-                    <section id="banner-section">
-                            <div class="main">
-                                <div class="slider slider-style-01">
-                                    <div class="slider-box-banner">
-                                        <div class="slider-text">
-                                            <h3>Bringing your digital dreams to life</h3>
-                                            <p>From concept to launch, we've got you covered</p>
-                                        </div>
-                                        <div class="slider-btn-style01">
-                                            <a href="javascript:;">explore binaro design</a>
-                                            <a href="javascript:;">view portfolio</a>
-                                        </div>
-                                    </div>
-                            <div><h3>2</h3></div>
-                            <div><h3>3</h3></div>
-                            <div><h3>4</h3></div>
-                            <div><h3>5</h3></div>
+        <section id="banner-section">
+            <div class="main">
+                <div class="slider slider-style-01">
+                    <div class="slider-box-banner">
+                        <div class="slider-text">
+                            <h3>Bringing your digital dreams to life</h3>
+                            <p>From concept to launch, we've got you covered</p>
                         </div>
+                        <div class="slider-btn-style01">
+                            <a href="javascript:;">explore binaro design</a>
+                            <a href="javascript:;">view portfolio</a>
                         </div>
-                    </section>
-                    <section id="brandlogo-section">
-                        <div class="ps_07">
-                            <div class="container">
-                                <div class="brand-headtext">
-                                    <h3>Trusted by</h3>
-                                </div>
-                                <div class="row">
-                                    <div class="col-lg-2 col-md-2 col-sm-6">
-                                        <div class="logo-block">
-                                            <img src="assets\images\elements\larky-logo.png" class="brndlogos img-fluid">
-                                        </div>
-                                    </div>   
-                                    <div class="col-lg-2 col-md-2 col-sm-6">
-                                        <div class="logo-block">
-                                            <img src="assets\images\elements\lunch-check-logo.png" class="brndlogos img-fluid">
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-3 col-md-2 col-sm-6">
-                                        <div class="logo-block">
-                                            <img src="assets\images\elements\confidein-logo.png" class="brndlogos img-fluid">
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-3 col-md-2 col-sm-6">
-                                        <div class="logo-block">
-                                            <img src="assets\images\elements\Waveritas_Logo.png" class="brndlogos img-fluid">
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-2 col-md-2 col-sm-6">
-                                        <div class="logo-block">
-                                            <img src="assets\images\elements\sihltal-thumbnail.png" class="brndlogos">
-                                        </div>
-                                    </div>
-                                    
-                                </div>
+                    </div>
+                    <div><h3>2</h3></div>
+                    <div><h3>3</h3></div>
+                    <div><h3>4</h3></div>
+                    <div><h3>5</h3></div>
+                </div>
+            </div>
+        </section>
+        <section id="brandlogo-section">
+            <div class="ps_07">
+                <div class="container">
+                    <div class="brand-headtext">
+                        <h3>Trusted by</h3>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-2 col-md-2 col-sm-6">
+                            <div class="logo-block">
+                                <img src="assets\images\elements\larky-logo.png" class="brndlogos img-fluid" />
                             </div>
                         </div>
-                    </section>
+                        <div class="col-lg-2 col-md-2 col-sm-6">
+                            <div class="logo-block">
+                                <img src="assets\images\elements\lunch-check-logo.png" class="brndlogos img-fluid" />
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-2 col-sm-6">
+                            <div class="logo-block">
+                                <img src="assets\images\elements\confidein-logo.png" class="brndlogos img-fluid" />
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-2 col-sm-6">
+                            <div class="logo-block">
+                                <img src="assets\images\elements\Waveritas_Logo.png" class="brndlogos img-fluid" />
+                            </div>
+                        </div>
+                        <div class="col-lg-2 col-md-2 col-sm-6">
+                            <div class="logo-block">
+                                <img src="assets\images\elements\sihltal-thumbnail.png" class="brndlogos" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="intro-section">
+            <div class="ps015">
+                <div class="conatiner">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12">
+                            <div class="overflow-text">
+                                <h4><span class="oflwtext-left">why</span><span class="circle-shad"></span><span class="oflwtext-right">binaro</span></h4>
+                            </div>
+                            <div class="intro-text-block">
+                                <h2>
+                                    Building<br />
+                                    <span class="head-style2">experiences</span><br />
+                                    that inspire
+                                </h2>
+                            </div>
+                            <div class="intro-desc">
+                                <p>
+                                    Website design, development, and marketing agency.<br />
+                                    We are passionate about creating exceptional digital experiences<br />
+                                    that empower businesses to reach their full potential.<br />
+                                    With years of experience, we have developed a proven process for<br />
+                                    delivering high-quality solutions tailored to our clients' unique needs.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 d-flex justify-content-center">
+                <div class="heading-style02 fade-in">
+                    <div class="heading-style02-img">
+                        <img src="assets\images\icons\Vector.png" class="hd-styimg-02 img-fluid" />
+                    </div>
+                    <a href="jascript:;">About us</a>
+                </div>
+            </div>
+        </div>
+        <section id="customize-section">
+            <div class="ps015">
+                <div class="conatiner">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12">
+                            <div class="cus-overflow-text">
+                                <h4><span class="oflwtext-left custext-left">How</span><span class="circle-shad cus-cicleshad"></span> <span class="cutext-right">we are m</span></h4>
+                            </div>
+                            <div class="intro-text-block">
+                                <h2>
+                                    Customized <br />
+                                    <span class="head-style2">solutions</span><br />
+                                    drive results
+                                </h2>
+                            </div>
+                            <div class="intro-desc">
+                                <p>
+                                    Four key areas that offer endless possibilities
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row text-center ps30">
+                        <div class="col-lg-2 col-md-2 col-sm-2"></div>
+                        <div class="col-lg-2 col-md-2 col-sm-2">
+                            <div class="head-styl03">
+                                <h1>1</h1>
+                            </div>
+                            <div class="description-customize des-cus-clr-gre">
+                                <h4>design</h4>
+                            </div>
+                        </div>
+                        <div class="col-lg-2 col-md-2 col-sm-2">
+                            <div class="head-styl03">
+                                <h1>2</h1>
+                            </div>
+                            <div class="description-customize des-cus-clr-blu">
+                                <h4>development</h4>
+                            </div>
+                        </div>
+                        <div class="col-lg-2 col-md-2 col-sm-2">
+                            <div class="head-styl03">
+                                <h1>3</h1>
+                            </div>
+                            <div class="description-customize des-cus-clr-red">
+                                <h4>media</h4>
+                            </div>
+                        </div>
+                        <div class="col-lg-2 col-md-2 col-sm-2">
+                            <div class="head-styl03">
+                                <h1>4</h1>
+                            </div>
+                            <div class="description-customize des-cus-clr-sky">
+                                <h4>coming soon</h4>
+                            </div>
+                        </div>
+                        <div class="col-lg-2 col-md-2 col-sm-2"></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 d-flex justify-content-center">
+                            <div class="heading-style02 fade-in">
+                                <div class="heading-style02-img">
+                                    <img src="assets\images\icons\Vector.png" class="hd-styimg-02 img-fluid" />
+                                </div>
+                                <a href="jascript:;">request callback</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="transforming-section">
+            <div class="ps015">
+                <div class="conatiner">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12">
+                            <div class="cus-overflow-text clr-red">
+                                <h4><span class="oflwtext-left custext-left">What</span><span class="circle-shad cus-cicleshad"></span> <span class="cutext-right"> Binaro i</span></h4>
+                            </div>
+                            <div class="intro-text-block tranform-head">
+                                <h2>
+                                    Transforming <br/>
+                                    <span class="head-style2">ideas into</span><br />
+                                    digital realities
+                                </h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row text-center ps30 ps-bt60">
+                    <div class="col-lg-2 col-md-2 col-sm-2"></div>
+                        <div class="col-lg-4 col-md-4 col-sm-4 pdlr0">
+                            <img src="assets\images\mix\1.jpg" class="img-fluid img-margbtm">
+                            <img src="assets\images\mix\3.jpg" class="img-fluid">
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-4 pdlr0">
+                         <div class="mar-top">   
+                        <img src="assets\images\mix\2.jpg" class="img-fluid img-margbtm">
+                        <img src="assets\images\mix\4.jpg" class="img-fluid">
+                        </div>
+                        </div>
+                        <div class="col-lg-2 col-md-2 col-sm-2"></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 d-flex justify-content-center">
+                            <div class="heading-style02 fade-in">
+                                <div class="heading-style02-img">
+                                    <img src="assets\images\icons\Vector.png" class="hd-styimg-02 img-fluid" />
+                                </div>
+                                <a href="jascript:;">Get a quote</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
 
         <!-- Footer -->
         <?php include("includes/footer.php") ?>
@@ -74,6 +227,5 @@
 
         <!-- Scripts -->
         <?php include("includes/scripts.php") ?>
-
     </body>
 </html>
