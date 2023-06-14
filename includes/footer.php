79735df46eebbf1c2b61fcea629c1564980745e1
Syed haris <haris.syed@binaro.io>
2023-06-14 22:47:57 +0500

    footer has been developed

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,188 @@
-<footer></footer>
+<footer>
+    <div class="footer_inner">
+        <div class="container">
+            <div class="footer_cpt">
+                <div class="row d-flex align-items-center">
+                    <div class="col-lg-8 col-md-8 col-sm-12">
+                        <div class="ft_cpttextblock">
+                            <h3>
+                                Are you uneasy about<br />
+                                the use of <b>AI tools?</b>
+                            </h3>
+                        </div>
+                        <div class="ftcpt_btn service_btn00">
+                            <a href="javascript:;">Let Us help you out</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-4">
+                        <div class="ft_cptimgblock">
+                            <img src="assets\images\elements\robot.png" class="hd-styimg-02 robot_img img-fluid" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer_main">
+        <div class="footer_mian_inner">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 col-sm-12 d-flex align-items-center">
+                        <div class="ft_logo">
+                            <img src="assets\images\elements\footerlogo.png" class="hd-styimg-02 ft_logo_img img-fluid" />
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-3">
+                        <div class="footer_icon_box">
+                            <div class="icon_img">
+                                <img src="assets\images\icons\office.png" class="hd-styimg-02 ft_logo_img img-fluid" />
+                            </div>
+                            <div class="icon_heading">
+                                <h4>Office</h4>
+                            </div>
+                            <div class="icon_des">
+                                <p>
+                                    15 Vyzantiou, 1st Floor, Office<br />
+                                    101 Chryseleousa, Strovolos 2064<br />
+                                    Nicosia, Cyprus
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-3">
+                        <div class="footer_icon_box">
+                            <div class="icon_email_img">
+                                <img src="assets\images\icons\emailimg.png" class="hd-styimg-02 ft_logo_img img-fluid" />
+                            </div>
+                            <div class="icon_heading">
+                                <h4>Email Us</h4>
+                            </div>
+                            <div class="icon_mail">
+                                <a href="mailto:service@binaro.io">service@binaro.io</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="ft_space">
+                    <div class="row">
+                        <div class="col mx-1">
+                            <div class="ft_lastcol border_blue">
+                                <div class="ft_col_heading headclr_blue">
+                                    <h4>
+                                        Binaro<br />
+                                        group
+                                    </h4>
+                                </div>
+                                <div class="ftmain_link">
+                                    <ul>
+                                        <li><a href="javascript:;">Home</a></li>
+                                        <li><a href="javascript:;">Units</a></li>
+                                        <li><a href="javascript:;">About Us </a></li>
+                                        <li><a href="javascript:;">Portfolio</a></li>
+                                        <li><a href="javascript:;">Resources</a></li>
+                                        <li><a href="javascript:;">Partner Program</a></li>
+                                        <li><a href="javascript:;">Contact Us</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col mx-1">
+                            <div class="ft_lastcol border_green">
+                                <div class="ft_col_heading headclr_green">
+                                    <h4>
+                                        Binaro<br />
+                                        Design
+                                    </h4>
+                                </div>
+                                <div class="ftmain_link">
+                                    <ul>
+                                        <li><a href="javascript:;">Home</a></li>
+                                        <li><a href="javascript:;">Our Services</a></li>
+                                        <li><a href="javascript:;">Our Work</a></li>
+                                        <li><a href="javascript:;">Partner Program</a></li>
+                                        <li><a href="javascript:;">Idea Contest</a></li>
+                                        <li><a href="javascript:;">Contact Us</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col mx-1">
+                            <div class="ft_lastcol border_skyblue">
+                                <div class="ft_col_heading headclr_skyblue">
+                                    <h4>
+                                        Binaro<br />
+                                        Development
+                                    </h4>
+                                </div>
+                                <div class="ftmain_link">
+                                    <ul>
+                                        <li><a href="javascript:;">Home</a></li>
+                                        <li><a href="javascript:;">Our Services</a></li>
+                                        <li><a href="javascript:;">Our Work</a></li>
+                                        <li><a href="javascript:;">Partner Program</a></li>
+                                        <li><a href="javascript:;">Idea Contest</a></li>
+                                        <li><a href="javascript:;">Contact Us</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col mx-1">
+                            <div class="ft_lastcol border_seagreen">
+                                <div class="ft_col_heading headclr_seagreen">
+                                    <h4>
+                                        Binaro<br />
+                                        security
+                                    </h4>
+                                </div>
+                                <div class="ftmain_link">
+                                    <ul>
+                                        <li><a href="javascript:;">Coming Soon</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col mx-1">
+                            <div class="ft_lastcol border_red">
+                                <div class="ft_col_heading headclr_red">
+                                    <h4>
+                                        Binaro<br />
+                                        Media
+                                    </h4>
+                                </div>
+                                <div class="ftmain_link">
+                                    <ul>
+                                        <li><a href="javascript:;">Home</a></li>
+                                        <li><a href="javascript:;">Our Services</a></li>
+                                        <li><a href="javascript:;">Our Work</a></li>
+                                        <li><a href="javascript:;">Partner Program</a></li>
+                                        <li><a href="javascript:;">Idea Contest</a></li>
+                                        <li><a href="javascript:;">Contact Us</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="copy_right_sect">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12">
+                            <div class="copyright_links">
+                                <ul>
+                                    <li><a href="javascript:;">Careers</a></li>
+                                    <li><a href="javascript:;">Newsroom</a></li>
+                                    <li><a href="javascript:;">Blogs</a></li>
+                                    <li><a href="javascript:;">FAQs</a></li>
+                                    <li><a href="javascript:;">Privacy Policy </a></li>
+                                    <li><a href="javascript:;"> Terms and Conditions</a></li>
+                                </ul>
+                            </div>
+                            <div class="copyright_block">
+                                <p>© Copyright 2023 Binaro. All Rights Reserved</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
--- includes/module-section.php
+++ includes/module-section.php
@@ -1,14 +1,14 @@
 <section id="cta-module">   
             <div class="cta-module-space">
-            <div class="conatiner">
-                <div class="row">
-                    <div class="col-lg-12 col-md-12 col-sm-12">
-                        <div class="overflow_heading">
+            <div class="overflow_heading">
                             <marquee direction="right" scrollamount="10"><h4>Think BIG <span class="anim-blue">Innovative ideas</span></h4></marquee>
                             <marquee direction="left" scrollamount="10"><h4>brand <span class="anim-green">bespoke solutions</span></h4></marquee>
                             <marquee direction="right" scrollamount="10"><h4><span class="anim-red">ideas</span> technology inspire</h4></marquee>
                             <marquee direction="left" scrollamount="10"><h4>web design <psan class="anim-watergreen">creativity</span></h4></marquee>
                         </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12">
                     <div class="heading-style02 fade-in btn-size">
                                 <div class="heading-style02-img">
                                     <img src="assets\images\icons\Vector1.png" class="hd-styimg-02 img-fluid">
@@ -19,4 +19,4 @@
                 </div>
             </div>
             </div>
-        </section>
+</section>
