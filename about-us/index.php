e4b459d40400a3951e4715c9ba91950a70c32627
Syed haris <haris.syed@binaro.io>
2023-06-20 21:59:01 +0500

    transformation hover updated

--- about-us/index.php
+++ about-us/index.php
@@ -1,91 +1,54 @@
-<?php include("includes/var.php") ?>
+<?php include("../includes/var.php") ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <title>Home | Binaro</title>
-        <?php include("includes/compatibility.php") ?>
-        <?php include("includes/styles.php") ?>
+        <?php include("../includes/compatibility.php") ?>
+        <?php include("../includes/styles.php") ?>
     </head>
     <body>
-        <?php include("includes/header.php") ?>
-        <section id="banner-section">
-            <div class="main">
-                <div class="slider slider-style-01">
-                    <div class="slider-box-banner">
-                        <div class="slider-text">
-                            <h3>Bringing your digital dreams to life</h3>
-                            <p>From concept to launch, we've got you covered</p>
-                        </div>
-                        <div class="slider-btn-style01">
-                            <a href="javascript:;">explore binaro design</a>
-                            <a href="javascript:;">view portfolio</a>
-                        </div>
-                    </div>
-                    <div><h3>2</h3></div>
-                    <div><h3>3</h3></div>
-                    <div><h3>4</h3></div>
-                    <div><h3>5</h3></div>
-                </div>
-            </div>
-        </section>
-        <section id="brandlogo-section">
-            <div class="ps_07">
+        <?php include("../includes/header.php") ?>
+        <section id="abt-banner-section">
+            <div class="abt-bg">
                 <div class="container">
-                    <div class="brand-headtext">
-                        <h3>Trusted by</h3>
-                    </div>
-                    <div class="row">
-                        <div class="col-lg-2 col-md-2 col-sm-6">
-                            <div class="logo-block">
-                                <img src="assets\images\elements\larky-logo.png" class="brndlogos img-fluid" />
+                    <div class="row  justify-content-center">
+                        <div class="col-lg-6 col-md-6 col-md-12">
+                            <div class="heading-style03-img">
+                                <img src="assets\images\icons\Vector.png" class="hd-styimg-03 img-fluid">
+                            </div>
+                            <div class="abt-text-block">
+                            <h2>About Binaro</h2>   
+                            <p>Binaro Group is your precise, customized, and economical partner for Design, Development, and Media Services. We are more than just a service provider; we believe in thinking big, being innovative, and embracing challenges to develop smart and customized solutions that solve problems in unexpected ways. Discover how Binaro Group can help you achieve your digital goals and make a positive impact in your success story.  </p>
                             </div>
                         </div>
-                        <div class="col-lg-2 col-md-2 col-sm-6">
-                            <div class="logo-block">
-                                <img src="assets\images\elements\lunch-check-logo.png" class="brndlogos img-fluid" />
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-md-2 col-sm-6">
-                            <div class="logo-block">
-                                <img src="assets\images\elements\confidein-logo.png" class="brndlogos img-fluid" />
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-md-2 col-sm-6">
-                            <div class="logo-block">
-                                <img src="assets\images\elements\Waveritas_Logo.png" class="brndlogos img-fluid" />
-                            </div>
-                        </div>
-                        <div class="col-lg-2 col-md-2 col-sm-6">
-                            <div class="logo-block">
-                                <img src="assets\images\elements\sihltal-thumbnail.png" class="brndlogos" />
+                        <div classa="col-lg-6 col-md-6 col-md-12">
+                            <div class="abt-img-block">
+                                <img src="assets\images\mix\abt-banner-img.png" class="img-fluid" />
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
+
         <section id="intro-section">
             <div class="ps015">
                 <div class="building-overflow-text overflow-text">
-                    <marquee width="120%" direction="right" scrollamount="12"><h4>Why Binaro</h4></marquee>
+                    <marquee width="120%" direction="right" scrollamount="12"><h4>OUR vision</h4></marquee>
                 </div>
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-12 col-md-12 col-sm-12">
                             <div class="intro-text-block">
                                 <h2>
-                                    Building<br />
-                                    <span class="head-style2">experiences</span><br />
-                                    that inspire
+                                binaro<br />
+                                    <span class="head-style2">vision</span><br />
+                                    statement
                                 </h2>
                             </div>
                             <div class="intro-desc">
                                 <p>
-                                    Website design, development, and marketing agency.<br />
-                                    We are passionate about creating exceptional digital experiences<br />
-                                    that empower businesses to reach their full potential.<br />
-                                    With years of experience, we have developed a proven process for<br />
-                                    delivering high-quality solutions tailored to our clients' unique needs.
+                                We want to be seen as an infinite provider of cutting-edge digital solutions<br> that attracts, connects, supports and enables cause-driven individuals and<br> companies in pursuing their ambitious boundary-moving goals.
                                 </p>
                             </div>
                         </div>
@@ -99,23 +62,23 @@
                     <div class="heading-style02-img">
                         <img src="assets\images\icons\Vector.png" class="hd-styimg-02 img-fluid" />
                     </div>
-                    <a href="jascript:;">About us</a>
+                    <a href="jascript:;">in More detail</a>
                 </div>
             </div>
         </div>
         <section id="customize-section">
             <div class="ps015">
                 <div class="cus-overflow-text overflow-text">
-                    <marquee direction="right" scrollamount="12"><h4>How we are making a difference</h4></marquee>
+                    <marquee direction="right" scrollamount="12"><h4>OUR MISSION</h4></marquee>
                 </div>
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-12 col-md-12 col-sm-12">
                             <div class="intro-text-block">
                                 <h2>
-                                    Customized <br />
-                                    <span class="head-style2">solutions</span><br />
-                                    drive results
+                                    Our <br />
+                                    <span class="head-style2">mission</span><br />
+                                    statement
                                 </h2>
                             </div>
                             <div class="intro-desc">
@@ -190,80 +153,18 @@
                         </div>
                     </div>
                     <div class="row text-center ps30 ps-bt60">
-                        <div class="col-lg-12 col-md-12 col-sm-21 pdlr0">
-                            <div class="row">
-                                <div class="col-lg-6 col-md-6 col-sm-6 pdlr0">
-                                   <div class="overlap-box hight_cus"> 
-                                    <img src="assets\images\mix\1.jpg" class="img-fluid img-margbtm" />
-                                    <div class="text_block_overlap_hover">
-                                    <div class="top-heading">
-                                        <h3>Eat Well And Do Good</h3>
-                                        <p>Description here</p>
-                                        </div>
-                                        <div class="top-heading bottom_heading">
-                                            <h2>branding<br> web design<br> web development</h2>
-                                            <div class="service_btn02">
-                                                <a href="javascript:;">View case study</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                   </div>
-                                </div>
-                                <div class="col-lg-6 col-md-6 col-sm-6 pdlr0">
-                                   <div class="overlap-box"> 
-                                   <img src="assets\images\mix\3.jpg" class="img-fluid" />
-                                    <div class="text_block_overlap_hover">
-                                    <div class="top-heading">
-                                        <h3>Eat Well And Do Good</h3>
-                                        <p>Description here</p>
-                                        </div>
-                                        <div class="top-heading bottom_heading">
-                                            <h2>branding<br> web design<br> web development</h2>
-                                            <div class="service_btn02">
-                                                <a href="javascript:;">View case study</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                   </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-lg-6 col-md-6 col-sm-6 pdlr0">
-                                   <div class="overlap-box hight_cus"> 
-                                    <img src="assets\images\mix\2.jpg" class="img-fluid img-margbtm" />
-                                    <div class="text_block_overlap_hover">
-                                    <div class="top-heading">
-                                        <h3>Eat Well And Do Good</h3>
-                                        <p>Description here</p>
-                                        </div>
-                                        <div class="top-heading bottom_heading">
-                                            <h2>branding<br> web design<br> web development</h2>
-                                            <div class="service_btn02">
-                                                <a href="javascript:;">View case study</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                   </div>
-                                </div>
-                                <div class="col-lg-6 col-md-6 col-sm-6 pdlr0">
-                                   <div class="overlap-box"> 
-                                   <img src="assets\images\mix\4.jpg" class="img-fluid" />
-                                    <div class="text_block_overlap_hover">
-                                        <div class="top-heading">
-                                        <h3>Eat Well And Do Good</h3>
-                                        <p>Description here</p>
-                                        </div>
-                                        <div class="top-heading bottom_heading">
-                                            <h2>branding<br> web design<br> web development</h2>
-                                            <div class="service_btn02">
-                                                <a href="javascript:;">View case study</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                   </div>
-                                </div>
+                        <div class="col-lg-2 col-md-2 col-sm-2"></div>
+                        <div class="col-lg-4 col-md-4 col-sm-4 pdlr0">
+                            <img src="assets\images\mix\1.jpg" class="img-fluid img-margbtm" />
+                            <img src="assets\images\mix\3.jpg" class="img-fluid" />
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-4 pdlr0">
+                            <div class="mar-top">
+                                <img src="assets\images\mix\2.jpg" class="img-fluid img-margbtm" />
+                                <img src="assets\images\mix\4.jpg" class="img-fluid" />
                             </div>
                         </div>
+                        <div class="col-lg-2 col-md-2 col-sm-2"></div>
                     </div>
                     <div class="row">
                         <div class="col-lg-12 col-md-12 col-sm-12 d-flex justify-content-center">
@@ -278,7 +179,7 @@
                 </div>
             </div>
         </section>
-        <?php include("includes/module-section.php") ?>
+        <?php include("../includes/module-section.php") ?>
         <section id="AI-section">
             <div class="ps015">
                 <div class="aicus-overflow-text ai_text_animnat overflow-text">
@@ -331,15 +232,15 @@
                 </div>
             </div>
         </section>
-        <?php include("includes/service_cpt.php") ?>
-        <?php include("includes/tab_section.php") ?>
+        <?php include("../includes/service_cpt.php") ?>
+        <?php include("../includes/tab_section.php") ?>
         <!-- Footer -->
-        <?php include("includes/footer.php") ?>
+        <?php include("../includes/footer.php") ?>
 
         <!-- Default Popup -->
-        <?php include("includes/popup.php") ?>
+        <?php include("../includes/popup.php") ?>
 
         <!-- Scripts -->
-        <?php include("includes/scripts.php") ?>
+        <?php include("../includes/scripts.php") ?>
     </body>
 </html>
--- includes/styles.php
+++ includes/styles.php
@@ -1,3 +1,4 @@
 <link rel="stylesheet" type="text/css" href="assets/css/plugin.min.css">
 <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+<link rel="stylesheet" type="text/css" href="assets/css/aboutstyle.css">
