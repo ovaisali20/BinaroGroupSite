667aba41fdff0e887d3807d9babe625b20eed110
Ovais <MAAZ@DESKTOP-0SCPMPC>
2023-08-06 18:35:27 +0500

    uploading

---
+++ career/index.php
@@ -0,0 +1,73 @@
+<?php include("../includes/var.php") ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Career | Binaro</title>
+        <?php include("../includes/compatibility.php") ?>
+        <?php include("../includes/styles.php") ?>
+    </head>
+    <body>
+
+        <?php include("../includes/header.php") ?>
+
+        <section class="banner style-2 bg-color-primary bg-detail-1" style="background-image: url(assets/images/bg/banner-1.jpg);">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-7">
+                        <div class="abt-text-block text-center">
+                            <h2 class="heading-style-1">Join Our Time</h2>
+                            <p>Join Binaro Group and unlock a world of career possibilities with our employee-centric approach, offering remote work, flexible hours, a collaborative ecosystem, and a voice for every team member.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="spacing__x">
+            <div class="marquee-text">
+                <marquee width="120%" direction="right" scrollamount="12"><h4>Why <span class="word-gap"></span>Binaro Why <span class="word-gap"></span>Binaro Why<span class="word-gap"></span> Binaro Why<span class="word-gap"></span> Binaro Why <span class="word-gap"></span>Binaro Why<span class="word-gap"></span> Binaro</h4></marquee>
+                <div class="intro-text-block mb-3">
+                    <h2>Why<br /> <span class="head-style2">Join</span><br />Binaro</h2>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-10">
+                        <div class="intro-desc">
+                            <p>Binaro has been delivering premium IT-enabled business solutions to a wide array of client organizations for over four decades. We have remained at the cutting edge of enterprise technology by making employee excellence our top priority. We believe in cultivating a working environment that supports innovation and creative thinking</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="spacing__x">
+            <div class="marquee-text">
+                <marquee width="120%" direction="right" scrollamount="12"><h4>open <span class="word-gap"></span>position <span class="word-gap"></span>in <span class="word-gap"></span> binaro<span class="word-gap"></span> Binaro open <span class="word-gap"></span>position <span class="word-gap"></span>in <span class="word-gap"></span> binaro</h4></marquee>
+                <div class="intro-text-block mb-3">
+                    <h2>Open<br /> <span class="head-style2">Position</span><br />In Binaro</h2>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-10">
+                        <div class="intro-desc">
+                            <p>Website design, development, and marketing agency. We are passionate about creating exceptional digital experiences that empower businesses to reach their full potential. With years of experience, we have developed a proven process for delivering high-quality solutions tailored to our clients' unique needs.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Footer -->
+        <div class="pad-top">
+            <?php include("../includes/footer.php") ?>
+        </div>
+
+        <!-- Default Popup -->
+        <?php include("../includes/popup.php") ?>
+
+        <!-- Scripts -->
+        <?php include("../includes/scripts.php") ?>
+    </body>
+</html>
--- case-studies/index.php
+++ case-studies/index.php
@@ -1,99 +1,32 @@
-    <?php include("includes/var.php") ?>
+<?php include("../includes/var.php") ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Home | Binaro</title>
-        <?php include("includes/compatibility.php") ?>
-        <?php include("includes/styles.php") ?>
+        <title>Case Studies | Binaro</title>
+        <?php include("../includes/compatibility.php") ?>
+        <?php include("../includes/styles.php") ?>
     </head>
     <body class="home-page">
 
-        <?php include("includes/header.php") ?>
+        <?php include("../includes/header.php") ?>
 
-        <section id="banner-section">
-            <div class="main">
-                <div class="slider slider-style-01">
-                    <div class="slider-box-banner">
-                        <div class="slider-text">
-                            <h3>Bringing your digital dreams to life</h3>
-                            <p>From concept to launch, we've got you covered</p>
-                        </div>
-                        <div class="slider-btn-style01">
-                            <a href="javascript:;">explore binaro design</a>
-                            <a href="javascript:;">view portfolio</a>
-                        </div>
-                    </div>
-                    <div><h3>2</h3></div>
-                    <div><h3>3</h3></div>
-                    <div><h3>4</h3></div>
-                    <div><h3>5</h3></div>
-                </div>
-            </div>
-        </section>
-
-        <section id="brandlogo-section">
-            <div class="ps_07">
-                <div class="container">
-                    <div class="brand-headtext">
-                        <h3>Trusted by</h3>
-                    </div>
-                    <div class="row">
-                        <div class="col-lg-2 col-md-2 col-sm-6">
-                            <div class="logo-block">
-                                <img src="assets/images/elements/larky-logo.png" class="brndlogos img-fluid" />
-                            </div>
-                        </div>
-                        <div class="col-lg-2 col-md-2 col-sm-6">
-                            <div class="logo-block">
-                                <img src="assets/images/elements/lunch-check-logo.png" class="brndlogos img-fluid" />
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-md-2 col-sm-6">
-                            <div class="logo-block">
-                                <img src="assets/images/elements/confidein-logo.png" class="brndlogos img-fluid" />
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-md-2 col-sm-6">
-                            <div class="logo-block">
-                                <img src="assets/images/elements/Waveritas_Logo.png" class="brndlogos img-fluid" />
-                            </div>
-                        </div>
-                        <div class="col-lg-2 col-md-2 col-sm-6">
-                            <div class="logo-block">
-                                <img src="assets/images/elements/sihltal-thumbnail.png" class="brndlogos" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <section class="spacing__x pb-0">
-            <div class="marquee-text">
-                <marquee width="120%" direction="right" scrollamount="12"><h4>Who <span class="word-gap"></span>Binaro</h4></marquee>
-                <div class="intro-text-block mb-3">
-                    <h2>Building <br /> <span class="head-style2">experiences</span><br />that inspire</h2>
-                </div>
-            </div>
+        <section class="banner style-2 bg-detail-1" style="background-image: url(assets/images/bg/banner-1.jpg);">
             <div class="container">
-                <div class="row">
-                    <div class="col-lg-12 col-md-12 col-sm-12">
-                        <div class="intro-desc">
-                            <p>We want to be seen as an infinite provider of cutting-edge digital solutions<br /> that attracts, connects, supports and enables cause-driven individuals and<br /> companies in pursuing their ambitious boundary-moving goals.</p>
+                <div class="row justify-content-center">
+                    <div class="col-lg-6 col-md-6 col-md-12">
+                        <div class="abt-text-block">
+                            <h2 class="heading-style-1">Case Studies</h2>
+                            <p>At Binaro Group, we believe in the power of real-world results. That's why our Case Studies are not just stories, but powerful accounts of how our services have truly made a difference for our clients. From overcoming challenges to achieving breakthrough goals, our case studies are a testament to our commitment to excellence.</p>
                         </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="text-center mb-110 mt-50">
-                            <a href="about-us" class="btn-style-1">About Us</a>
+                    <div classa="col-lg-6 col-md-6 col-md-12">
+                        <div class="abt-img-block">
+                            <img src="assets/images/banner/banner-case-study.png" />
                         </div>
                     </div>
                 </div>
             </div>
         </section>
-
-        <?php include("includes/our-services.php") ?>
 
         <section class="transforming-section spacing__x">
             <div class="marquee-text">
@@ -291,53 +224,17 @@
             </div>
         </section>
 
-        <?php include("includes/module-section.php") ?>
+        <?php include("../includes/cta-1.php") ?>
 
-        <section class="ai-section spacing__x">
-            <div class="marquee-text">
-                <marquee direction="right" scrollamount="12"><h4>ai with binaro ai with binaro ai with binaro ai with binaro ai with binaro ai with binaro</h4></marquee>
-                <div class="intro-text-block">
-                    <h2>Ai <br /> <span class="head-style2">do it very</span><br />professionally</h2>
-                </div>
-            </div>
-            <div class="container">
-                <div class="container cus_width">
-                    <div class="row ps30 ps-bt60 border_width2 space_p">
-                        <div class="col-lg-6 col-md-6 col-sm-6 d-flex flex-wrap align-content-center">
-                            <div class="ai_descblock">
-                                <p><span class="clr_black">Lorem ipsum <span class="text_bg">dolor</span> sit amet, consectetur adipiscing elit. <span class="text_bg"> Fusce</span> tempus nisl non est posuere lacinia. Nullam accumsan odio <span class="text_bg"> non felis varius</span> efficitur. Class aptent taciti</span> sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc at dolor vestibulum, dapibus lacus quis, maximus sapien. Morbi aliquam risus ac facilisis fermentum.</p>
-                            </div>
-                            <div class="voice_btn">
-                                <a href="javascript:;">AI voice generator <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-md-6 col-sm-6">
-                            <img src="assets/images/elements/ai_img.png" class="hd-styimg-02 img-fluid img_size_align img_marg_botm" />
-                            <img src="assets/images/elements/sound.png" class="hd-styimg-02 img-fluid img_size_align" />
-                        </div>
-                    </div>
-                </div>
-                <div class="row d-flex justify-content-center">
-                    <div class="col-lg-12 col-md-12 col-sm-12">
-                        <div class="text-center mt-50">
-                            <a href="javascript:;" class="btn-style-1">learn more</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <?php include("includes/cta-1.php") ?>
-
-        <?php include("includes/our-blogs.php") ?>
+        <?php include("../includes/module-section.php") ?>
         
         <!-- Footer -->
-        <?php include("includes/footer.php") ?>
+        <?php include("../includes/footer.php") ?>
 
         <!-- Default Popup -->
-        <?php include("includes/popup.php") ?>
+        <?php include("../includes/popup.php") ?>
 
         <!-- Scripts -->
-        <?php include("includes/scripts.php") ?>
+        <?php include("../includes/scripts.php") ?>
     </body>
 </html>
